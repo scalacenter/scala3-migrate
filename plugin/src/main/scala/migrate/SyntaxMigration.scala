c2425249f534c95d7cc76bb52ce4b537c26b2eb0
Adrien Piquerez <adrien.piquerez@gmail.com>
2023-08-29 15:43:45 +0200

    Rework syntax migration

--- plugin/src/main/scala/migrate/SyntaxMigration.scala
+++ plugin/src/main/scala/migrate/SyntaxMigration.scala
@@ -16,22 +16,22 @@ private[migrate] object SyntaxMigration {
     val configs   = migrationConfigs.value
     val projectId = thisProject.value.id
     val sv        = scalaVersion.value
+    val log       = streams.value.log
 
     if (!sv.startsWith("2.13."))
       throw new MessageOnlyException(Messages.notScala213(sv, projectId))
 
+    log.info(s"${BOLD}Starting migration of syntax in $projectId / [${configs.mkString(", ")}]${RESET}")
+
     Def.task {
-      val logger          = streams.value.log
+      val log             = streams.value.log
       val _               = configs.map(_ / compile).join.value
       val allScala2Inputs = configs.map(_ / scala2Inputs).join.value
-
-      logger.info(welcome(projectId, configs.map(_.id)))
 
       for {
         (scala2Inputs, config) <- allScala2Inputs.zip(configs)
         if scala2Inputs.unmanagedSources.nonEmpty
       } Try {
-        logger.info(scala2Inputs.unmanagedSources.mkString("\n"))
         migrateAPI.migrateSyntax(
           scala2Inputs.unmanagedSources.asJava,
           scala2Inputs.semanticdbTarget,
@@ -40,41 +40,12 @@ private[migrate] object SyntaxMigration {
         )
       } match {
         case scala.util.Success(_) =>
-          logger.info(success(projectId, config.id))
-        case Failure(exception) =>
-          logger.err(error(projectId, config.id, exception))
+        case Failure(cause) =>
+          log.error(s"Failed migration of syntax in $projectId / $config")
+          throw cause
       }
 
-      logger.info(nextCommand(projectId))
+      log.info(s"Migration of syntax in $projectId / [${configs.mkString(", ")}] succeeded.")
     }
   }
-
-  private def welcome(projectId: String, configs: Seq[String]): String =
-    s"""|
-        |${BOLD}We are going to fix some syntax incompatibilities in $projectId / ${configs
-      .mkString("[", ", ", "]")}${RESET}
-        |
-        |""".stripMargin
-
-  private def success(projectId: String, config: String) =
-    s"""|
-        |The syntax incompatibilities have been fixed in $projectId / $config
-        |
-        |""".stripMargin
-
-  private def error(projectId: String, config: String, ex: Throwable): String =
-    s"""|
-        |Failed fixing the syntax in $projectId / $config
-        |${ex.getMessage()}
-        |${ex.getStackTrace.mkString("\n")}
-        |
-        |""".stripMargin
-
-  private def nextCommand(projectId: String): String =
-    s"""|
-        |You can now commit the change!
-        |Then you can run the next command:
-        |
-        |migrateTypes $projectId
-        |""".stripMargin
 }
